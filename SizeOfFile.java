89f04be818e03710851669c38084c6958ec7c4d7
Akhileswar reddy Gajjala <131006031+Akhileswar7@users.noreply.github.com>
2024-08-25 19:52:15 +0530

    Add files via upload

---
+++ SizeOfFile.java
@@ -0,0 +1,22 @@
+package com.vedait.assignments.fileHandling;
+
+import java.io.File;
+
+public class SizeOfFile {
+	
+	public static void main(String[] args) {
+		
+		String path="C:\\Users\\gajja\\Downloads\\filehandling\\file.txt";
+		printSize(path);
+	}
+
+	private static void printSize(String path) {
+		File file = new File(path);
+		long bytes=file.length();
+		System.out.println(bytes+" bytes");
+		System.out.println((double)bytes/1024+" kb");
+		System.out.println(((double)bytes/1024)/1024+" mb");
+		
+		
+	}
+}
